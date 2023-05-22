cbea6371164ff7ac80d933e4e9c7def6ed455958
Cem GÖKSU <127553434+cemgoksu35@users.noreply.github.com>
2023-05-22 21:13:43 +0300

    cem
    Encapsulation

---
+++ Models/Musteri.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Encapsulation.Models
+{
+    public class Musteri // model
+    {
+        public string Adi { get; set; } // Bütün özellik isimleri büyük harfle başlar. 
+        public string Soyadi { get; set; }
+        public byte Yasi { get; set; }
+        public string CepTelefonu { get; set; }
+        public string Adresi { get; set; }
+        // public string _adresi; Alan ismi küçük harfle başlar ve önünde "_" ile kullanılır.
+        public bool KadinMi { get; set; }
+        public string Cinsiyet => KadinMi ? "Kadın" : "Erkek"; // Lambda kullanımı
+        /* {
+             get
+             {
+                 if (KadinMi)
+                     return "Kadın";
+
+                     return "Erkek";
+             }
+         }*/
+        public string Unvan => KadinMi ? "Bayan" : "Bay";
+
+
+        string _kartno; //(Alan) Bu verinin gizli kalması ve dışardan erişilmemesi lazım.
+
+        public string KartNo //(Özellik) Özellik oluşturalım
+        {
+            set
+            {
+                _kartno = value; // atanan değeri _kartno kısmına set ediyoruz.
+            }
+            get
+            {
+                return "**** **** **** " + _kartno.Substring(15); // Burada kart şu şekilde görünecek **** **** **** 3456 
+            }
+        }
+         
+
+        //1. Yöntem Method ile
+        /*
+        public void SetKartNo(string KartNo) // Kart numarasını aldık atadık ve sakladığımız kart no üstünden işlem yapılacak.
+        {
+            _kartno = KartNo;
+        }
+
+        public string GetKartNo()//1234 5678 9102 3456
+        {
+            return "**** **** **** " + _kartno.Substring(15); // Burada kart şu şekilde görünecek **** **** **** 3456 
+        }
+        */
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Encapsulation.Models; // Namespace eklememiz lazım 
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Adi = "Cem";
+            musteri.Soyadi = "Göksu";
+            musteri.Yasi = 32;
+            musteri.CepTelefonu = "05322781811";
+            musteri.KartNo = "1234 5678 9012 3456";
+            musteri.KadinMi = false;
+            Console.WriteLine($"Müşteri Adı ve Soyadı : {musteri.Unvan} {musteri.Adi} {musteri.Soyadi} Yaşı : {musteri.Yasi} Cinsiyeti : {musteri.Cinsiyet} Kart Numarası : {musteri.KartNo}");
+            
+        }
+    }
+}
